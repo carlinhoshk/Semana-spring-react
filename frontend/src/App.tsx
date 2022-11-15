b1f2c7ebc6b82916880a53500277845019741ebf
carlos Oliveira <carlosmdohk@gmail.com>
2022-11-15 00:50:01 -0300

    subindo os arquivos front/back versionando menos :fire:

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+
+            <SalesCard />
+
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
